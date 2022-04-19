21991cc0d314c536ef0da55da446265a77370579
Brian Garton <gartonb@gmail.com>
2022-04-19 17:20:51 +0700

    First Page Done

--- restaurant-page/src/components/Header.js
+++ restaurant-page/src/components/Header.js
@@ -11,25 +11,31 @@ const Header = () => {
       setClick(!click)
   }
   return (
-    <div className="header-con">
-        <div className="logo-container">
-            <h1>.my-restaurant</h1>
-        </div>
-        <div className="links-container">
-            <div className="list-item">
-                <a href="#">Menu</a>
+        <div className="header-con">
+            <div className="logo-container">
+                <h1>.my-restaurant</h1>
             </div>
-            <div className="list-item">
-                <a href="#">About</a>
+            <div className="links-container">
+                <div className="list-item">
+                    <a href="#">Menu</a>
+                </div>
+                <div className="list-item">
+                    <a href="#">About</a>
+                </div>
+                <div className="list-item">
+                    <a href="#">Contact</a>
+                </div>
+                <div className="list-item">
+                    <a href="#">Testimonials</a>
+                </div>
+                <div className="list-item">
+                    <a href="#">Contact</a>
+                </div>
             </div>
-            <div className="list-item">
-                <a href="#">Contact</a>
+            <div className="mobile-menu" onClick={handleMobileMenu}>
+                {click ? (<FiMenu />) : (<FiX />)}
             </div>
         </div>
-        <div className="mobile-menu" onClick={handleMobileMenu}>
-            {click ? (<FiMenu />) : (<FiX />)}
-        </div>
-    </div>
   )
 }
 
